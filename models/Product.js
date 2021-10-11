49febfba2a7bfa9fc3baf4c76ace46e52c43d205
fuatguzel <fuatguzel754@gmail.com>
2021-10-11 19:02:07 +0300

    Create Models

---
+++ models/Product.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose')
+const {Schema} = mongoose.Schema
+
+const ProductSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    description: { 
+      type: String,
+      required: true
+    },
+    image: { 
+      type: String,
+      required: true
+    },
+    categories: { 
+      type: Array
+    },
+    size: { 
+      type: String
+    },
+    color: { 
+      type: String
+    },
+    price: { 
+      type: Number,
+      required: true
+    },
+  },
+  // CreatedAt and UpdatedAt
+  {timestamps: true}
+)
+
+module.exports = mongoose.model('Product', ProductSchema)
