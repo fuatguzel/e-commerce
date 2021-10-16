08d50009dd92cdd6f5f0fce40b1130aae9bd8b5e
fuatguzel <fuatguzel754@gmail.com>
2021-10-16 22:58:02 +0300

    Add cart controller.

--- routes/cart.js
+++ routes/cart.js
@@ -1,70 +1,32 @@
 const express = require('express');
 const router = express.Router();
-const Cart = require('../models/cart')
 const {
   verifyToken,
   verifyTokenAndAuthorization,
   verifyTokenAndAdmin
 } = require('./verifyToken')
+const {
+  createNewCart,
+  updateCart,
+  deleteCart,
+  getSingleCart,
+  getAllCarts
+} = require('../controllers/cart');
 
 // Create a new Cart instance.
-router.post('/', verifyToken, async (req, res) => {
-  const newCart = new Cart(req.body)
-
-  try {
-    const savedCart = await newCart.save()
-    res.status(200).json(savedCart)
-  } catch (error) {
-    res.status(500).json(error)
-  }
-})
+router.post('/', verifyToken, createNewCart)
 
 // Update cart.
-router.put('/:id', verifyTokenAndAuthorization, async (req, res) => {
-  try {
-    const updatedCart = await Cart.findByIdAndUpdate(
-      req.params.id, {
-        $set: req.body
-      }, {
-        new: true
-      })
-    res.status(200).json(updatedCart)
-  } catch (error) {
-    res.status(500).json(error)
-  }
-})
+router.put('/:id', verifyTokenAndAuthorization, updateCart)
 
 // Delete the cart from the database.
-router.delete('/:id', verifyTokenAndAuthorization, async (req, res) => {
-  try {
-    const cart = await Cart.findByIdAndDelete(req.params.id)
-    res.status(200).json("Cart successfully deleted.")
-  } catch (error) {
-    res.status(500).json(error)
-  }
-})
+router.delete('/:id', verifyTokenAndAuthorization, deleteCart)
 
 // Get the user cart data from the database.
-router.get('/find/:userId', verifyTokenAndAuthorization, async (req, res) => {
-  try {
-    const cart = await Cart.findOne({
-      userId: req.params.userId
-    })
-    res.status(200).json(cart)
-  } catch (error) {
-    res.status(500).json(error)
-  }
-})
+router.get('/find/:userId', verifyTokenAndAuthorization, getSingleCart)
 
 // Get all cart items from the database.
-router.get('/', verifyTokenAndAdmin, async (req, res) => {
-  try {
-    const carts = await Cart.find()
-    res.status(200).json(carts)
-  } catch (error) {
-    res.status(500).json(error)
-  }
-})
+router.get('/', verifyTokenAndAdmin, getAllCarts)
 
 
 module.exports = router
